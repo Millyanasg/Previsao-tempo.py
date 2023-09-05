e6b1f3d44b440142b364c443089e43e45f9c3fb5
Millyana <Millyanamello@gmail.com>
2023-09-05 14:32:42 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Aplicativo de Previsão do Tempo
+Este é um aplicativo Flask que fornece a previsão do tempo para uma cidade específica usando a API OpenWeatherMap.
+
+**Pré-requisitos**
+Python 3.8
+pip
+Uma chave API do OpenWeatherMap. Obtenha aqui.
+Instalação e Configuração
+
+**Clone o repositório:**
+git clone [URL_DO_SEU_REPOSITÓRIO]
+cd [NOME_DO_DIRETÓRIO_DO_PROJETO]
+
+**Crie um ambiente virtual:**
+python -m venv myenv
+Ative o ambiente virtual:
+
+**No Windows**
+.\myenv\Scripts\Activate
+**No Linux ou macOS:**
+source myenv/bin/activate
+
+**Instale as dependências:**
+pip install -r requirements.txt
+
+**Configurando a Chave API:**
+Copie o arquivo .env.example para .env.
+Abra o arquivo .env e defina OPEN_WEATHER_API_KEY com sua chave API do OpenWeatherMap.
+
+**Executando o Aplicativo:**
+Para iniciar o aplicativo, simplesmente execute:
+python app.py
+Acesse http://localhost:5000/ no seu navegador para ver o aplicativo em ação.
+
+
+# Criador
+Millyana Souza. 
+
---
+++ app.py
@@ -0,0 +1,61 @@
+
+from flask import Flask, render_template, request, flash
+from decouple import config
+import requests
+
+app = Flask(__name__)
+app.secret_key = 'some_secret_key'
+API_KEY = config('OPEN_WEATHER_API_KEY')
+API_URL = "http://api.openweathermap.org/data/2.5/weather?q={{}}&appid={API_KEY}"
+
+def get_temperature_details(celsius: float) -> str:
+    if 28 <= celsius <= 32:
+        return {'icon': 'melting.png', 'text': 'Certamente está todo mundo começando a derreter!'}
+    elif 32 <= celsius <= 45:
+        return {'icon': 'extreme_heat.png', 'text': 'Observamos aqui uma mostra gràtis do inferno!'}
+    elif 10 <= celsius < 16:
+        return {'icon': 'extreme_cold.png', 'text': 'A Elsa passou e deixou o LERIGOOU!'}
+    elif 16 <= celsius < 20:
+        return {'icon': 'moderate_cold.png', 'text': 'Que tal um chocolate quente e Netflix ?'}
+    elif 20 <= celsius < 28:
+        return {'icon': 'pleasant.png', 'text': 'Levanta da cadeira e vai levar seu Pet pra passear, o clima ta bom pra isso!'}
+    else:
+        return {'icon': 'default_icon.png', 'text': 'Quem dera se todos os dias fossem assim, não acha ?'}
+
+def get_weather(city: str) -> dict:
+    response = requests.get(API_URL.format(city))
+    if response.status_code == 200:
+        data = response.json()
+        kelvin = data['main']['temp']
+        celsius = kelvin - 273.15
+        temp_details = get_temperature_details(celsius)
+        icon_name = temp_details['icon']
+        temp_text = temp_details['text']
+        
+        return {
+            'city': data['name'],
+            'temperature': "{:.1f}°C".format(celsius),
+             'icon': icon_name,
+            'temp_text': temp_text,
+        }
+    else:
+        return None
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    weather = None
+    if request.method == 'POST':
+        city = request.form.get('city')
+        if not city:
+            flash('Por favor, insira o nome de uma cidade', 'warning')
+        else:
+            weather = get_weather(city)
+            if not weather:
+                flash('Não foi possível obter a previsão para essa cidade', 'danger')
+    else:
+        weather = get_weather('Rio de Janeiro')
+
+    return render_template('home.html', weather=weather)
+
+if __name__ == '__main__':
+    app.run(debug=True)
